e2844f2a1b0dcaafd1e776c8a3e6e00d9192a9bb
Serkan-Ucakcioglu <serkanuckc123@gmail.com>
2022-08-21 00:22:47 +0300

    select box css add and indent fixed

--- src/Components/Section/ProductList.jsx
+++ src/Components/Section/ProductList.jsx
@@ -42,13 +42,26 @@ export default function ProductList() {
 
   return (
     <div className="container">
-      <select value={select} onChange={(e) => setSelect(e.target.value)}>
-        <option value="choose">Choose</option>
-        <option value="electronics">electronics</option>
-        <option value="jewelery">jewelery</option>
-        <option value="men's clothing">men's clothing</option>
-        <option value="women's clothing">women's clothing</option>
-      </select>
+      <div className="flex flex-col items-center">
+        <label
+          for="category"
+          className="block mt-2 text-sm font-medium text-gray-900 dark:text-gray-400"
+        >
+          Select an option
+        </label>
+        <select
+          id="category"
+          className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-1/2 p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+          value={select}
+          onChange={(e) => setSelect(e.target.value)}
+        >
+          <option value="choose">Choose</option>
+          <option value="electronics">electronics</option>
+          <option value="jewelery">jewelery</option>
+          <option value="men's clothing">men's clothing</option>
+          <option value="women's clothing">women's clothing</option>
+        </select>
+      </div>
       <ul className="grid grid-flow-row-dense grid-cols-4 grid-rows-6 ">
         {select === "choose" &&
           entities.map((product) => (
