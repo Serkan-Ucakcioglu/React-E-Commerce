9f6135ee6a4d15103fb79af0204059cc7f318a4e
Serkan-Ucakcioglu <serkanuckc123@gmail.com>
2022-08-20 21:19:57 +0300

    product list column

--- src/Components/Section/ProductList.jsx
+++ src/Components/Section/ProductList.jsx
@@ -1,7 +1,7 @@
 import { useEffect } from "react";
 import { useDispatch, useSelector } from "react-redux";
 import { getProduct } from "../../Store/productSlice";
-import {NavLink} from "react-router-dom"
+import { NavLink } from "react-router-dom";
 
 export default function ProductList() {
   const dispatch = useDispatch();
@@ -18,20 +18,20 @@ export default function ProductList() {
       <ul className="grid grid-flow-row-dense grid-cols-4 grid-rows-6">
         {entities.map((product) => (
           <li>
-            <NavLink to={'/home'}  key={product.id}>
-            <div className="p-10 flex direction-column justify-center items-center max-w-sm mt-5 h-96 mr-2 bg-white rounded-lg border border-gray-200 shadow-md dark:bg-gray-800 dark:border-gray-700">
-              <img
-                src={product.image}
-                alt={product.title}
-                className="cursor-pointer h-4/6 w-4/6"
-              />
-              <a href="#">
-                <h5 className="mb-2 text-xl mt-2 font-semibold tracking-tight text-gray-900 dark:text-white">
-                  {product.title}
-                </h5>
-              </a>
-            </div>
-          </NavLink>
+            <NavLink to={"/home"} key={product.id}>
+              <div className="p-10 flex flex-col justify-center items-center max-w-sm mt-5 h-96 mr-2 bg-white rounded-lg border border-gray-200 shadow-md dark:bg-gray-800 dark:border-gray-700">
+                <img
+                  src={product.image}
+                  alt={product.title}
+                  className="cursor-pointer h-4/6 w-4/6"
+                />
+                <a href="#">
+                  <h5 className="mb-2 text-xl mt-2 font-semibold tracking-tight text-gray-900 dark:text-white">
+                    {product.title}
+                  </h5>
+                </a>
+              </div>
+            </NavLink>
           </li>
         ))}
       </ul>
