32c9a788763544305d20605d1ec817eabb14b59d
Serkan-Ucakcioglu <serkanuckc123@gmail.com>
2022-08-21 16:09:45 +0300

    login page create

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,8 @@ import { Route, Routes } from "react-router-dom";
 import Header from "./Components/Header/Header";
 import HomePage from "./Pages/HomePage";
 import DetailsPage from "./Pages/DetailsPage";
+import Login from "./Components/Forms/Login";
+import Register from "./Components/Forms/Register";
 
 function App() {
   return (
@@ -11,6 +13,8 @@ function App() {
         <Route path="/header" element={<Header />} />
         <Route path="/" element={<HomePage />} />
         <Route path="/detail/:id" element={<DetailsPage />} />
+        <Route path="/login" element={<Login/>} />
+        <Route path="/register" element={<Register/>} />
       </Routes>
     </div>
   );
--- src/Components/Header/Header.jsx
+++ src/Components/Header/Header.jsx
@@ -74,20 +74,20 @@ function Header() {
             </a>
           </nav>
           <div className="hidden md:flex items-center justify-end md:flex-1 lg:w-0">
-            <a
-              href="#"
+            <NavLink
+              to={"/register"}
               className="whitespace-nowrap text-base font-medium text-gray-500 hover:text-gray-900"
             >
               {" "}
-              Sign in{" "}
-            </a>
-            <a
-              href="#"
+              Register{" "}
+            </NavLink>
+            <NavLink
+              to={"/login"}
               className="ml-8 whitespace-nowrap inline-flex items-center justify-center px-4 py-2 border border-transparent rounded-md shadow-sm text-base font-medium text-white bg-indigo-600 hover:bg-indigo-700"
             >
               {" "}
-              Sign up{" "}
-            </a>
+              Login{" "}
+            </NavLink>
           </div>
         </div>
       </div>
