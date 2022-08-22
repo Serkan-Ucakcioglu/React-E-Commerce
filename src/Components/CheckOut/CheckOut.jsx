9093189e1cf81aac0a92ef77b85c0a3d3a32bb0b
Serkan-Ucakcioglu <serkanuckc123@gmail.com>
2022-08-22 12:50:20 +0300

    items text space

--- src/Components/CheckOut/CheckOut.jsx
+++ src/Components/CheckOut/CheckOut.jsx
@@ -26,7 +26,7 @@ function CheckOut() {
           <div className="w-3/4 bg-white px-10 py-10">
             <div className="flex justify-between border-b pb-8">
               <h1 className="font-semibold text-2xl">Shopping Cart</h1>
-              <h2 className="font-semibold text-2xl">{basket.length}Items</h2>
+              <h2 className="font-semibold text-2xl">{basket.length} Items</h2>
             </div>
             <div className="flex mt-10 mb-5">
               <h3 className="font-semibold text-gray-600 text-xs uppercase w-2/5">
