c83c286ba633e283da88bb690a2e93f6449a1214
Serkan-Ucakcioglu <serkanuckc123@gmail.com>
2022-08-22 17:10:52 +0300

    create HashRouter

--- src/index.js
+++ src/index.js
@@ -2,15 +2,15 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import "./index.css";
 import App from "./App";
-import { BrowserRouter } from "react-router-dom";
+import { HashRouter } from "react-router-dom";
 import { Provider } from "react-redux";
 import store from "./Store";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
-  <BrowserRouter>
+  <HashRouter>
     <Provider store={store}>
     <App />
     </Provider>
-  </BrowserRouter>
+  </HashRouter>
 );
