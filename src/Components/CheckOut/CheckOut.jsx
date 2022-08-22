470ff5b7ab60b0df46fe4375f018a206d3fb79e5
Serkan-Ucakcioglu <serkanuckc123@gmail.com>
2022-08-22 15:30:18 +0300

    responsive css and indent

--- src/Components/CheckOut/CheckOut.jsx
+++ src/Components/CheckOut/CheckOut.jsx
@@ -28,8 +28,8 @@ function CheckOut() {
         </h1>
       )}
       {count > 0 && (
-        <div className="flex shadow-md my-10 res">
-          <div className="w-3/4 bg-white px-10 py-10 wll">
+        <div className="flex shadow-md my-10 res bg-gray-600">
+          <div className="w-3/4 bg-neutral-50 00 px-10 py-10 wll">
             <div className="flex justify-between border-b pb-8 wll">
               <h1 className="font-semibold text-2xl">Shopping Cart</h1>
               <h2 className="font-semibold text-2xl">
@@ -112,12 +112,15 @@ function CheckOut() {
             </NavLink>
           </div>
 
-          <div id="summary" className="w-1/4 wll px-8 py-10 justify-between">
-            <h1 className="font-semibold text-2xl border-b pb-8 justify-between">
+          <div
+            id="summary"
+            className="w-1/4 wll px-8 py-10 justify-between border-l-2 border-white"
+          >
+            <h1 className="font-semibold s text-white text-2xl border-b pb-8 justify-between">
               Order Summary
             </h1>
 
-            <div className="mt-3">
+            <div className="mt-3 text-white">
               <label className="font-medium inline-block mb-3 text-sm uppercase">
                 Shipping
               </label>
@@ -128,7 +131,7 @@ function CheckOut() {
             <div className="py-10">
               <label
                 htmlFor="promo"
-                className="font-semibold inline-block mb-3 text-sm uppercase"
+                className="font-semibold text-white inline-block mb-3 text-sm uppercase"
               >
                 Promo Code
               </label>
@@ -136,14 +139,14 @@ function CheckOut() {
                 type="text"
                 id="promo"
                 placeholder="Enter your code"
-                className="p-2 text-sm w-full border-2 border-black rounded"
+                className="p-2 text-sm w-full rounded"
               />
             </div>
             <button className="bg-red-500 hover:bg-red-600 px-5 py-2 text-sm text-white uppercase rounded">
               Apply
             </button>
             <div className="border-t mt-8">
-              <div className="flex font-semibold justify-between py-6 text-sm uppercase">
+              <div className="flex font-semibold text-white justify-between py-6 text-sm uppercase">
                 <span>Total cost</span>
                 <span>
                   $
