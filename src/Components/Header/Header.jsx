b777d25cdccbd6cc9bd69eb3384bb8b3800c6b3c
Serkan-Ucakcioglu <serkanuckc123@gmail.com>
2022-08-22 15:56:36 +0300

    responsive fixed

--- src/Components/Header/Header.jsx
+++ src/Components/Header/Header.jsx
@@ -87,12 +87,12 @@ function Header() {
                     alt="basket"
                   />
                   {count > 0 && (
-          <div className="w-4">
-            <strong className="bg-gray-600 rounded-full w-5 px-2 text-white">
-              {count}
-            </strong>
-          </div>
-        )}
+                    <div className="w-4">
+                      <strong className="bg-gray-600 rounded-full w-5 px-2 text-white">
+                        {count}
+                      </strong>
+                    </div>
+                  )}
                 </NavLink>
                 <div className="-mr-2">
                   {show && (
@@ -140,44 +140,45 @@ function Header() {
               </div>
               <div className="mt-6"></div>
             </div>
-            { show === true && (
+            {show === true && (
               <>
-            <div className="py-6 px-5 space-y-6">
-                <div className="flex flex-col mb-2">
-                  <NavLink
-                    className="text-base font-medium text-gray-500 hover:text-gray-900"
-                    data-mdb-ripple="true"
-                    data-mdb-ripple-color="light"
-                    to={"/"}
-                  >
-                    Home
-                  </NavLink>
-                  <div className="text-base font-medium text-gray-900 hover:text-gray-700">
-                    Pricing
-                  </div>
+                <div className="py-6 px-5 space-y-6">
+                  <div className="flex flex-col mb-2">
+                    <NavLink
+                      className="text-base mb-2 font-medium text-black hover:text-gray-900 border-2 border-gray-400 rounded"
+                      data-mdb-ripple="true"
+                      data-mdb-ripple-color="light"
+                      to={"/"}
+                    >
+                      Home
+                    </NavLink>
+                    <div className="text-base mb-2 font-medium text-gray-900 hover:text-gray-700 border-2 border-gray-400 rounded">
+                      Pricing
+                    </div>
 
-                  <div className="text-base font-medium text-gray-900 hover:text-gray-700">
-                    Docs{" "}
+                    <div className="text-base mb-2 font-medium text-gray-900 hover:text-gray-700 border-2 border-gray-400 rounded">
+                      About
+                    </div>
+                  </div>
+                  <div>
+                    <NavLink
+                      to={"/login"}
+                      className="w-full flex items-center justify-center px-4 py-2 border border-transparent rounded-md shadow-sm text-base font-medium text-white bg-indigo-600 hover:bg-indigo-700"
+                    >
+                      Giriş Yap
+                    </NavLink>
+                    <p className="mt-6 text-center text-base font-medium text-gray-500">
+                      Existing customer?
+                      <NavLink
+                        to={"/register"}
+                        className="text-indigo-600 hover:text-indigo-500 ml-1"
+                      >
+                        Register
+                      </NavLink>
+                    </p>
                   </div>
                 </div>
-                <div>
-                  <NavLink
-                    to={"/login"}
-                    className="w-full flex items-center justify-center px-4 py-2 border border-transparent rounded-md shadow-sm text-base font-medium text-white bg-indigo-600 hover:bg-indigo-700"
-                  >
-                    Giriş Yap
-                  </NavLink>
-                  <p className="mt-6 text-center text-base font-medium text-gray-500">
-                    Existing customer?
-                    <NavLink
-                      to={"/register"}
-                      className="text-indigo-600 hover:text-indigo-500"
-                    >
-                      Register
-                    </NavLink>
-                  </p>
-            </div>
-                </div></>
+              </>
             )}
           </div>
         </div>
