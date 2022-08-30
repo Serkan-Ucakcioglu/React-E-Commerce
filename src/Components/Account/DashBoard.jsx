d8bcd001c837573dc9d21782fa7a433faf4d7af1
Serkan-Ucakcioglu <serkanuckc123@gmail.com>
2022-08-30 17:09:37 +0300

    indent and active class css

--- src/Components/Account/DashBoard.jsx
+++ src/Components/Account/DashBoard.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 import { logOut } from "../../Store/userAuth";
 import { useDispatch } from "react-redux";
-import { useNavigate, NavLink ,Outlet } from "react-router-dom";
+import { useNavigate, NavLink, Outlet } from "react-router-dom";
 
 function DashBoard() {
   const dispatch = useDispatch();
@@ -33,69 +33,6 @@ function DashBoard() {
                 </svg>
                 <span className="ml-3">Dashboard</span>
               </NavLink>
-            </li>
-            <li>
-              <button
-                type="button"
-                className="flex items-center p-2 w-full text-base font-normal text-gray-900 rounded-lg transition duration-75 group hover:bg-gray-100 dark:text-white dark:hover:bg-gray-700"
-                aria-controls="dropdown-example"
-                data-collapse-toggle="dropdown-example"
-              >
-                <svg
-                  aria-hidden="true"
-                  className="flex-shrink-0 w-6 h-6 text-gray-500 transition duration-75 group-hover:text-gray-900 dark:text-gray-400 dark:group-hover:text-white"
-                  fill="currentColor"
-                  viewBox="0 0 20 20"
-                  xmlns="http://www.w3.org/2000/svg"
-                >
-                  <path
-                    fillRule="evenodd"
-                    d="M10 2a4 4 0 00-4 4v1H5a1 1 0 00-.994.89l-1 9A1 1 0 004 18h12a1 1 0 00.994-1.11l-1-9A1 1 0 0015 7h-1V6a4 4 0 00-4-4zm2 5V6a2 2 0 10-4 0v1h4zm-6 3a1 1 0 112 0 1 1 0 01-2 0zm7-1a1 1 0 100 2 1 1 0 000-2z"
-                    clipRule="evenodd"
-                  ></path>
-                </svg>
-                <span className="flex ml-2  ml-3 text-left whitespace-nowrap">
-                  E-commerce
-                </span>
-                <svg
-                  className="w-6 h-6"
-                  fill="currentColor"
-                  viewBox="0 0 20 20"
-                  xmlns="http://www.w3.org/2000/svg"
-                >
-                  <path
-                    fillRule="evenodd"
-                    d="M5.293 7.293a1 1 0 011.414 0L10 10.586l3.293-3.293a1 1 0 111.414 1.414l-4 4a1 1 0 01-1.414 0l-4-4a1 1 0 010-1.414z"
-                    clipRule="evenodd"
-                  ></path>
-                </svg>
-              </button>
-              <ul id="dropdown-example" className="hidden py-2 space-y-2">
-                <li>
-                  <NavLink
-                    to={"/"}
-                    className="flex items-center p-2 w-full text-base font-normal text-gray-900 rounded-lg transition duration-75 group hover:bg-gray-100 dark:text-white dark:hover:bg-gray-700"
-                  >
-                    Products
-                  </NavLink>
-                </li>
-                <li>
-                  <NavLink
-                    to={"/"}
-                    className="flex items-center p-2  w-full text-base font-normal text-gray-900 rounded-lg transition duration-75 group hover:bg-gray-100 dark:text-white dark:hover:bg-gray-700"
-                  >
-                    Billing
-                  </NavLink>
-                </li>
-                <li>
-                  <NavLink
-                    to={"/"}
-                    className="flex items-center p-2  w-full text-base font-normal text-gray-900 rounded-lg transition duration-75 group hover:bg-gray-100 dark:text-white dark:hover:bg-gray-700"
-                  >
-                    Invoice
-                  </NavLink>
-                </li>
-              </ul>
             </li>
             <li>
               <NavLink
