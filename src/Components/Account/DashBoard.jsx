3254e02f65e751a2c161bcbab044536003cf30be
Serkan-Ucakcioglu <serkanuckc123@gmail.com>
2022-08-30 16:44:36 +0300

    dashboard side bar li fix

--- src/Components/Account/DashBoard.jsx
+++ src/Components/Account/DashBoard.jsx
@@ -55,7 +55,7 @@ function DashBoard() {
                     clipRule="evenodd"
                   ></path>
                 </svg>
-                <span className="flex-1 ml-3 text-left whitespace-nowrap">
+                <span className="flex ml-2  ml-3 text-left whitespace-nowrap">
                   E-commerce
                 </span>
                 <svg
@@ -75,7 +75,7 @@ function DashBoard() {
                 <li>
                   <NavLink
                     to={"/"}
-                    className="flex items-center p-2 pl-11 w-full text-base font-normal text-gray-900 rounded-lg transition duration-75 group hover:bg-gray-100 dark:text-white dark:hover:bg-gray-700"
+                    className="flex items-center p-2 w-full text-base font-normal text-gray-900 rounded-lg transition duration-75 group hover:bg-gray-100 dark:text-white dark:hover:bg-gray-700"
                   >
                     Products
                   </NavLink>
@@ -83,7 +83,7 @@ function DashBoard() {
                 <li>
                   <NavLink
                     to={"/"}
-                    className="flex items-center p-2 pl-11 w-full text-base font-normal text-gray-900 rounded-lg transition duration-75 group hover:bg-gray-100 dark:text-white dark:hover:bg-gray-700"
+                    className="flex items-center p-2  w-full text-base font-normal text-gray-900 rounded-lg transition duration-75 group hover:bg-gray-100 dark:text-white dark:hover:bg-gray-700"
                   >
                     Billing
                   </NavLink>
@@ -91,7 +91,7 @@ function DashBoard() {
                 <li>
                   <NavLink
                     to={"/"}
-                    className="flex items-center p-2 pl-11 w-full text-base font-normal text-gray-900 rounded-lg transition duration-75 group hover:bg-gray-100 dark:text-white dark:hover:bg-gray-700"
+                    className="flex items-center p-2  w-full text-base font-normal text-gray-900 rounded-lg transition duration-75 group hover:bg-gray-100 dark:text-white dark:hover:bg-gray-700"
                   >
                     Invoice
                   </NavLink>
@@ -112,7 +112,7 @@ function DashBoard() {
                 >
                   <path d="M5 3a2 2 0 00-2 2v2a2 2 0 002 2h2a2 2 0 002-2V5a2 2 0 00-2-2H5zM5 11a2 2 0 00-2 2v2a2 2 0 002 2h2a2 2 0 002-2v-2a2 2 0 00-2-2H5zM11 5a2 2 0 012-2h2a2 2 0 012 2v2a2 2 0 01-2 2h-2a2 2 0 01-2-2V5zM11 13a2 2 0 012-2h2a2 2 0 012 2v2a2 2 0 01-2 2h-2a2 2 0 01-2-2v-2z"></path>
                 </svg>
-                <span className="flex-1 whitespace-nowrap">Kanban</span>
+                <span className="flex ml-2  whitespace-nowrap">Kanban</span>
                 <span className="inline-flex justify-center items-center px-2  text-sm font-medium text-gray-800 bg-gray-200 rounded-full dark:bg-gray-700 dark:text-gray-300">
                   Pro
                 </span>
@@ -133,7 +133,7 @@ function DashBoard() {
                   <path d="M8.707 7.293a1 1 0 00-1.414 1.414l2 2a1 1 0 001.414 0l2-2a1 1 0 00-1.414-1.414L11 7.586V3a1 1 0 10-2 0v4.586l-.293-.293z"></path>
                   <path d="M3 5a2 2 0 012-2h1a1 1 0 010 2H5v7h2l1 2h4l1-2h2V5h-1a1 1 0 110-2h1a2 2 0 012 2v10a2 2 0 01-2 2H5a2 2 0 01-2-2V5z"></path>
                 </svg>
-                <span className="flex-1 whitespace-nowrap">Inbox</span>
+                <span className="flex ml-2  whitespace-nowrap">Inbox</span>
                 <span className="inline-flex justify-center items-center p-3 ml-3 w-3 h-3 text-sm font-medium text-blue-600 bg-blue-200 rounded-full dark:bg-blue-900 dark:text-blue-200">
                   3
                 </span>
@@ -157,7 +157,7 @@ function DashBoard() {
                     clipRule="evenodd"
                   ></path>
                 </svg>
-                <span className="flex-1 whitespace-nowrap">Users</span>
+                <span className="flex ml-2  whitespace-nowrap">Users</span>
               </NavLink>
             </li>
             <li>
@@ -178,7 +178,7 @@ function DashBoard() {
                     clipRule="evenodd"
                   ></path>
                 </svg>
-                <span className="flex-1  whitespace-nowrap">Products</span>
+                <span className="flex ml-2 whitespace-nowrap">Products</span>
               </NavLink>
             </li>
             <li>
@@ -199,7 +199,7 @@ function DashBoard() {
                     clipRule="evenodd"
                   ></path>
                 </svg>
-                <span className="flex-1 whitespace-nowrap">Sign In</span>
+                <span className="flex ml-2  whitespace-nowrap">Sign In</span>
               </NavLink>
             </li>
             <li>
@@ -220,7 +220,7 @@ function DashBoard() {
                     clipRule="evenodd"
                   ></path>
                 </svg>
-                <span className="flex-1 whitespace-nowrap">Sign Up</span>
+                <span className="flex ml-2  whitespace-nowrap">Sign Up</span>
               </NavLink>
             </li>
             <li>
