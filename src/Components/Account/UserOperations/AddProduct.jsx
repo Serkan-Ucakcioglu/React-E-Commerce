49c6ac72ad9012392393bf397195c63547cf95fc
Serkan-Ucakcioglu <serkanuckc123@gmail.com>
2022-08-31 16:31:55 +0300

    addproduct form validation

--- src/Components/Account/UserOperations/AddProduct.jsx
+++ src/Components/Account/UserOperations/AddProduct.jsx
@@ -1,9 +1,22 @@
 import React from "react";
+import { useForm } from "react-hook-form";
 
 function AddProduct() {
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm();
+  const onSubmit = (data) => {
+    console.log(data);
+  };
+
   return (
     <div className="container h-full flex items-center">
-      <form className="w-1/3 flex flex-col justify-center m-auto">
+      <form
+        onSubmit={handleSubmit(onSubmit)}
+        className="w-1/3 flex flex-col justify-center m-auto"
+      >
         <div className="mb-6">
           <label
             htmlFor="title"
@@ -14,11 +27,19 @@ function AddProduct() {
           <input
             type="text"
             id="title"
-            name="title"
+            {...register("title", {
+              required: true,
+              minLength: 5,
+              maxLength: 12,
+            })}
             className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
-            placeholder="name@flowbite.com"
-            required=""
+            placeholder="Product Title"
           />
+          {errors.title && (
+            <div className="text-red-500 text-left">
+              minimum length must be 5
+            </div>
+          )}
         </div>
         <div className="mb-6">
           <label
@@ -30,9 +51,14 @@ function AddProduct() {
           <input
             type="number"
             id="price"
+            {...register("price", { required: true, minNumber: 5 })}
             className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
-            required=""
           />
+          {errors.price && (
+            <div className="text-red-500 text-left">
+              minimum price must be 5
+            </div>
+          )}
         </div>
         <div className="mb-6">
           <label
@@ -44,10 +70,16 @@ function AddProduct() {
           <input
             type="text"
             id="description"
-            name="description"
+            {...register("description", {
+              required: true,
+              minLength: 8,
+              maxLength: 20,
+            })}
             className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
-            required=""
           />
+          {errors.description && (
+            <div className="text-red-500 text-left">description must be 8</div>
+          )}
         </div>
         <div className="mb-6">
           <label
@@ -58,27 +90,33 @@ function AddProduct() {
           </label>
           <input
             type="url"
-            name="image"
+            {...register("url", {
+              required: true,
+            })}
             id="image"
             className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
-            required=""
           />
+          {errors.url && (
+            <div className="text-red-500 text-left">minimum url </div>
+          )}
         </div>
         <div className="flex items-start mb-6">
           <div className="flex items-center h-5">
             <input
               id="remember"
               type="checkbox"
-              value=""
+              {...register("checkbox", { required: true })}
               className="w-4 h-4 bg-gray-50 rounded border border-gray-300 focus:ring-3 focus:ring-blue-300 dark:bg-gray-700 dark:border-gray-600 dark:focus:ring-blue-600 dark:ring-offset-gray-800"
-              required=""
             />
           </div>
           <label
             htmlFor="remember"
-            className="ml-2 text-sm font-medium text-gray-900 dark:text-gray-300"
+            className="ml-2 flex flex-col text-sm font-medium text-gray-900 dark:text-gray-300"
           >
             Remember me
+            {errors.checkbox && (
+              <div className="text-red-500 text-center">required check </div>
+            )}
           </label>
         </div>
         <button
