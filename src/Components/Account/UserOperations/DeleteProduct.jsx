bb40efd533695671eab885fa4aa4fd5534eeefb3
Serkan-Ucakcioglu <serkanuckc123@gmail.com>
2022-09-01 15:00:20 +0300

    delete product page form error message add

--- src/Components/Account/UserOperations/DeleteProduct.jsx
+++ src/Components/Account/UserOperations/DeleteProduct.jsx
@@ -31,11 +31,12 @@ function DeleteProduct() {
           <input
             type="text"
             id="productId"
-            {...register("id", { required: true })}
+            {...register("id", { required: "required  id!" })}
             className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
             placeholder="The id number of the product you want to delete"
             required=""
           />
+          <div className="text-left text-red-500">{errors.id?.message}</div>
         </div>
         <div className="flex items-start mb-6">
           <div className="flex items-center h-5">
