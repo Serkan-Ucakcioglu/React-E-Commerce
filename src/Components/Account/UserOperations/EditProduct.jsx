1e4dd8130bb0c8b4398de71759877b350b0e3bb0
Serkan Uçakcıoğlu <serkanuckc123@gmail.com>
2022-09-08 15:39:35 +0300

    edit product page ,alert add

--- src/Components/Account/UserOperations/EditProduct.jsx
+++ src/Components/Account/UserOperations/EditProduct.jsx
@@ -16,7 +16,9 @@ function EditProduct() {
     })
       .then((res) => res.json())
       .then((json) => console.log(json));
-    alert("api database işlemeye izin vermiyor console bak");
+      alert(`api database işlemeye izin vermiyor console bak
+      ${JSON.stringify(data)}
+    `);
   };
 
   return (
