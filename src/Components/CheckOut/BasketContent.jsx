15ce895eab43fc5409c8d01ed580dd9b6868437f
Serkan Uçakcıoğlu <serkanuckc123@gmail.com>
2023-03-07 14:09:49 +0300

    updated code

--- src/Components/CheckOut/BasketContent.jsx
+++ src/Components/CheckOut/BasketContent.jsx
@@ -2,6 +2,8 @@ import React from "react";
 import { NavLink } from "react-router-dom";
 import { useDispatch } from "react-redux";
 import { addBasket, remove, updateQuantity } from "../../Features/productSlice";
+import Decrement from "../../assets/Decrement";
+import Quantity from "../../assets/Quantity";
 function BasketContent({ product }) {
   const dispatch = useDispatch();
 
@@ -43,27 +45,18 @@ function BasketContent({ product }) {
         </div>
       </div>
       <div className="flex justify-center w-1/5 wll mr-2">
-        <svg
-          className="fill-current text-gray-600 w-3 cursor-pointer"
-          viewBox="0 0 448 512"
-          onClick={() => decrement(product)}
-        >
-          <path d="M416 208H32c-17.67 0-32 14.33-32 32v32c0 17.67 14.33 32 32 32h384c17.67 0 32-14.33 32-32v-32c0-17.67-14.33-32-32-32z" />
-        </svg>
+        <button onClick={() => decrement(product)}>
+          <Decrement />
+        </button>
 
         <input
           className="mx-2 border text-center w-8"
           type="text"
           value={product.quantity}
         />
-
-        <svg
-          className="fill-current text-gray-600 w-3 cursor-pointer"
-          viewBox="0 0 448 512"
-          onClick={() => quantity(product)}
-        >
-          <path d="M416 208H272V64c0-17.67-14.33-32-32-32h-32c-17.67 0-32 14.33-32 32v144H32c-17.67 0-32 14.33-32 32v32c0 17.67 14.33 32 32 32h144v144c0 17.67 14.33 32 32 32h32c17.67 0 32-14.33 32-32V304h144c17.67 0 32-14.33 32-32v-32c0-17.67-14.33-32-32-32z" />
-        </svg>
+        <button onClick={() => quantity(product)}>
+          <Quantity />
+        </button>
       </div>
       <span className="text-center w-1/5 font-semibold text-sm mls-2">
         ${product.quantity * product.price}
---
+++ src/assets/Decrement.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+
+function Decrement() {
+  return (
+    <>
+      <svg
+        className="fill-current text-gray-600 w-3 cursor-pointer"
+        viewBox="0 0 448 512"
+      >
+        <path d="M416 208H32c-17.67 0-32 14.33-32 32v32c0 17.67 14.33 32 32 32h384c17.67 0 32-14.33 32-32v-32c0-17.67-14.33-32-32-32z" />
+      </svg>
+    </>
+  );
+}
+
+export default Decrement;
