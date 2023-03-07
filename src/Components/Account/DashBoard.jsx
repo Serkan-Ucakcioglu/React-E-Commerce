b0e6a0f04df2b142ef4294261e6cbc4c3ca0e724
Serkan Uçakcıoğlu <serkanuckc123@gmail.com>
2023-03-07 14:14:29 +0300

    splitting

--- src/Components/Account/DashBoard.jsx
+++ src/Components/Account/DashBoard.jsx
@@ -1,5 +1,6 @@
 import { useDispatch } from "react-redux";
 import { useNavigate, NavLink, Outlet } from "react-router-dom";
+import Edit from "../../assets/Edit";
 import { logOut } from "../../Features/userAuth";
 
 function DashBoard() {
@@ -56,31 +57,7 @@ function DashBoard() {
                 to={"/account/edit-product"}
                 className="flex items-center p-2 text-base font-normal text-white rounded-lg dark:text-white hover:bg-gray-100 hover:text-black dark:hover:bg-gray-700"
               >
-                <svg
-                  width="24px"
-                  height="24px"
-                  viewBox="0 0 24 24"
-                  className="text-white"
-                  id="_24x24_On_Light_Edit"
-                  data-name="24x24/On Light/Edit"
-                  xmlns="http://www.w3.org/2000/svg"
-                >
-                  <rect
-                    className="text-white"
-                    id="view-box"
-                    width="24"
-                    height="24"
-                    fill="none"
-                  />
-                  <path
-                    id="Shape"
-                    className="text-white"
-                    d="M.75,17.5A.751.751,0,0,1,0,16.75V12.569a.755.755,0,0,1,.22-.53L11.461.8a2.72,2.72,0,0,1,3.848,0L16.7,2.191a2.72,2.72,0,0,1,0,3.848L5.462,17.28a.747.747,0,0,1-.531.22ZM1.5,12.879V16h3.12l7.91-7.91L9.41,4.97ZM13.591,7.03l2.051-2.051a1.223,1.223,0,0,0,0-1.727L14.249,1.858a1.222,1.222,0,0,0-1.727,0L10.47,3.91Z"
-                    transform="translate(3.25 3.25)"
-                    fill="white"
-                  />
-                </svg>
-
+                <Edit />
                 <span className="flex ml-3 whitespace-nowrap">Edit</span>
               </NavLink>
             </li>
