320616f2b171755953e6a189d1a27b70dc27e93e
Serkan Uçakcıoğlu <serkanuckc123@gmail.com>
2023-03-07 14:50:03 +0300

    updated

--- src/Components/Section/ProductSelect.jsx
+++ src/Components/Section/ProductSelect.jsx
@@ -11,7 +11,7 @@ function ProductSelect({ select, setSelect }) {
       </label>
       <select
         id="category"
-        className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-1/2 p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+        className="bg-gray-50 border rounded  border-gray-400 text-gray-900 text-sm  focus:ring-blue-500 focus:border-blue-500 block w-1/2 p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
         value={select}
         onChange={(e) => setSelect(e.target.value)}
       >
