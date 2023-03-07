b4bc4b558be72894ca9490214b77a160647f12c8
Serkan Uçakcıoğlu <serkanuckc123@gmail.com>
2023-03-07 14:45:06 +0300

    updated project

--- src/Pages/CheckOutPage.jsx
+++ src/Pages/CheckOutPage.jsx
@@ -1,11 +1,9 @@
 import React from "react";
 import CheckOut from "../Components/CheckOut/CheckOut";
-import Header from "../Components/Header/Header";
 
 function CheckOutPage() {
   return (
     <>
-      <Header />
       <CheckOut />
     </>
   );
--- src/Pages/HomePage.jsx
+++ src/Pages/HomePage.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import { Outlet } from "react-router-dom";
-import Header from "../Components/Header/Header";
+import Header from "../Components/Header/Head/Header";
 
 function HomePage() {
   return (
